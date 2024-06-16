23ba503b9e0a78ec7d3fe938734970dca34edfb6
AlinaChumachenko <103441578+AlinaChumachenko@users.noreply.github.com>
2024-06-16 19:46:08 +0300

    update structure

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+
+import "@/styles/globals.css";
+
+const MyApp = ({ Component, pageProps }) => (
+ 
+      <Component {...pageProps} />
+    
+);
+
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Zustand</h1>
+    </div>
+  );
+}
