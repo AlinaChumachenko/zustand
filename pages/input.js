4bae323062142eb90d2e04f17da11e65478abac4
AlinaChumachenko <103441578+AlinaChumachenko@users.noreply.github.com>
2024-06-19 00:43:00 +0300

    update file input use zustand

--- pages/input.js
+++ pages/input.js
@@ -1,18 +1,67 @@
 import { create } from "zustand";
 // import React, { useState } from "react";
 
+function square(number) {
+  return number ** 2;
+}
+function total(totalNumber) {
+  return totalNumber.length;
+}
+function calculateBirthYear(age) {
+  const currentYear = new Date().getFullYear();
+  return currentYear - age;
+}
+
 const useInputStore = create((set) => ({
   title: "",
   setTitle: (newTitle) => set({ title: newTitle }),
+  text: "",
+  setText: (newText) => set({ text: newText }),
+  number: "",
+  setNumber: (newNumber) => set({ number: newNumber }),
+  totalNumber: "",
+  setTotalNumber: (newTotalNumber) => set({ totalNumber: newTotalNumber }),
+  age: 0,
+  setAge: (newAge) => set({ age: newAge }),
 }));
 
 function Input() {
-  //   const [title, setTitle] = useState("");
-  const { title, setTitle } = useInputStore();
+  const {
+    title,
+    text,
+    number,
+    totalNumber,
+    age,
+    setTitle,
+    setText,
+    setNumber,
+    setTotalNumber,
+    setAge,
+  } = useInputStore();
 
   function handleChangeTitle(event) {
     setTitle(event.target.value);
-    console.log(event.target.value);
+    // console.log(event.target.value);
+  }
+
+  function handleChangeText(event) {
+    setText(event.target.value);
+    // console.log(event.target.value);
+  }
+
+  function handleChangeNumber(event) {
+    setNumber(event.target.value);
+    // console.log(event.target.value);
+  }
+
+  function handleChangeTotalNumber(event) {
+    setTotalNumber(event.target.value);
+    // console.log(event.target.value);
+  }
+
+  function handleChangeAge(event) {
+    setAge(event.target.value);
+    // console.log(event.target.value);
   }
 
   return (
@@ -24,6 +73,41 @@ function Input() {
         onChange={handleChangeTitle}
       />
       <p className="mb-4 text-blue-500"> title: {title}</p>
+
+      <input
+        className="px-4 py-2 mb-2 border border-gray-400 rounded-lg focus:outline-none focus:ring focus:ring-blue-400"
+        type="text"
+        value={text}
+        onChange={handleChangeText}
+      />
+      <p className="text-blue-500"> text: {text}</p>
+
+      <input
+        className="px-4 py-2 mb-2 border border-gray-400 rounded-lg focus:outline-none focus:ring focus:ring-blue-400"
+        type="text"
+        value={number}
+        onChange={handleChangeNumber}
+      />
+      {/* paragraph outputs the square of a number */}
+      <p className="text-blue-500"> square: {square(number)}</p>
+
+      <input
+        className="px-4 py-2 mb-2 border border-gray-400 rounded-lg focus:outline-none focus:ring focus:ring-blue-400"
+        type="text"
+        value={totalNumber}
+        onChange={handleChangeTotalNumber}
+      />
+      {/* paragraph outputs the number of digits entered */}
+      <p className="text-blue-500"> total: {total(totalNumber)}</p>
+
+      <input
+        className="px-4 py-2 mb-2 border border-gray-400 rounded-lg focus:outline-none focus:ring focus:ring-blue-400"
+        type="text"
+        value={age}
+        onChange={handleChangeAge}
+      />
+      {/* paragraph outputs the number of digits entered */}
+      <p className="text-blue-500"> year of birth: {calculateBirthYear(age)}</p>
     </div>
   );
 }
