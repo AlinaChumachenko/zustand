b2cd75723c946d4d286b87b6bb553b86e8c9b3c5
AlinaChumachenko <103441578+AlinaChumachenko@users.noreply.github.com>
2024-06-19 16:17:55 +0300

    file counterPlus update zustand

---
+++ pages/counterPlus.js
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { create } from "zustand";
+
+const useCounterStore = create((set) => ({
+  count: 0,
+  setCount: () => set((newCount) => ({ count: newCount.count + 1 })),
+}));
+
+function useCounter() {
+  const { count, setCount } = useCounterStore();
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setCount();
+    }, 1000);
+
+    return () => clearInterval(id);
+  }, [setCount]);
+
+  return count;
+}
+
+export default function Counter() {
+  const count = useCounter();
+
+  return <h1>Seconds passed: {count}</h1>;
+}
